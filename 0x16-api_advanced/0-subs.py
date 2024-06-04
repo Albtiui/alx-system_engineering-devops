d207f59c09931d026eced312d7eba4e9e0b5850d
Albtiui Mohamed <135072672+Albtiui@users.noreply.github.com>
2024-06-04 11:05:54 +0000

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,38 @@
-#!/usr/bin/python3
+'''#!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
 """
 
 import requests
 
+def number_of_subscribers(subreddit):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code >= 300:
+        return 0   
+    json_data = r.json()
+    data_dict = json_data.get('data')
+    return data_dict.get('subscribers')
+    '''
+#!/usr/bin/python3
+
+import requests as r
+
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Text"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
+    response = r.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
